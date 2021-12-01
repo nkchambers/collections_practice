0827d58a3466d03562fb131d2df197a34c309a91
nkchambers <nkchames@gmail.com>
2021-11-30 18:50:20 -0800

    Added random ice cream function

--- Program.cs
+++ Program.cs
@@ -11,26 +11,28 @@ namespace collections_practice
         // ---------------------------------------THREE BASIC ARRAYS----------------------------------------------
         
         //---------------------------Array 1-------------------------------
-            int[] numArray1;
-            numArray1 = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-            foreach (int num in numArray1) {
+            int[] numbers = new int[10];
+            for (var i = 0; i<numbers.Length; i++) {
                 
-                Console.WriteLine(numArray1[num]);
+                Console.WriteLine(numbers[i] = i);
             }
 
 
          //--------------------------Array 2-------------------------------
-            string[] nameArray2 = new string[] { "Tim", "Martin", "Nikki", "Sara" };
-            foreach (string name in nameArray2) {
+            string[] names = new string[] 
+            { 
+                "Tim", "Martin", "Nikki", "Sara" 
+            };
+
+            foreach (string name in names) {
                 
                 Console.WriteLine($"{name}");
             }
         
 
          //--------------------------Array 3----------------------------------
-            int[] numArray3;
-            numArray3 = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-            for (int i=0; i< numArray3.Length; i++) {
+            bool[] boolyz = new bool[10];
+            for (int i = 0; i < boolyz.Length; i++) {
                 if (i%2 == 0) {
                     Console.WriteLine("True");
                 }
@@ -47,13 +49,14 @@ namespace collections_practice
 
         //---------------------------------------------LIST OF FLAVORS----------------------------------------
             //C1 - Instantiate List of Ice Cream Flavors named flavors
-            List<string> flavors = new List<string>();
-
-            flavors.Add("Mint Chocolate Chip");
-            flavors.Add("Java Mocha Chip");
-            flavors.Add ("Rocky Road");
-            flavors.Add("French Vanilla");
-            flavors.Add ("Banana Split");
+            List<string> flavors = new List<string>()
+            {
+                "Mint Chocolate Chip",
+                "Java Mocha Chip", 
+                "Rocky Road", 
+                "French Vanilla", 
+                "Banana Split"
+            };
 
 
             //C2 - Output length (List_name.count) of flavors List (total count of ice cream flavors)
@@ -62,7 +65,7 @@ namespace collections_practice
 
             //C3 - Output third flavor in List, then remove value
             Console.WriteLine($"The third flavor in the list is {flavors[2]}.");
-            flavors.Remove("Rocky Road");
+            flavors.RemoveAt(2);
 
 
             //C4 - Output length (List_name.count) of flavors List (total count should be 1 less now)
@@ -81,17 +84,17 @@ namespace collections_practice
 
 
             //C2 - Add Key/Value Pairs to Dictionary (Keys - names, Values - ice cream flavors)
-            userInfo.Add("Tim", "Java Mocha Chip");
-            userInfo.Add("Martin", "French Vanilla");
-            userInfo.Add("Nikki", "Mint Chocolate Chip");
-            userInfo.Add("Sara", "Banana Split");
+            foreach(var name in names)
+                userInfo.Add(name, null);
 
+            // For each name key, select a random flavor from the flavor list above and store it as the value
+            Random rand = new Random();
+            foreach(var name in names)
+                userInfo[name] = (flavors[rand.Next(flavors.Count)]);
 
-            //C3 - Loop through dictionary and print out each user's name and their associated ice cream flavor
-            foreach (var entry in userInfo) {
-                
-                Console.WriteLine(entry.Key + " - " + entry.Value);
-            }
+            // Loop through the dictionary and print out each user's name and their associated ice cream flavor
+            foreach(var kp in userInfo)
+                Console.WriteLine($"{kp.Key}: {kp.Value}");
         }
     }
 }
